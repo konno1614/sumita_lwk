186222b8abda6c5360624a9e0761bca1a75c2aa9
konno <konno@buddying.jp>
2023-04-11 11:05:08 +0900

    修正

--- public-src/js/lib/Loading.js
+++ public-src/js/lib/Loading.js
@@ -1,5 +1,5 @@
 //ローディング画面の表示
 $(window).on('load',function(){
-  $("#loading").delay(1500).fadeOut('slow');
-  $("#loading_box").delay(1200).fadeOut('slow');
+  $("#loading").delay(500).fadeOut('slow');
+  $("#loading_box").delay(800).fadeOut('slow');
 });
